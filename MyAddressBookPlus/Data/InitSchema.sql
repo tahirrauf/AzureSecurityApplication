c8e8bfec383315c37fd5383bedf64251270a5d36
tahirrauf <tahir.abdul.rauf@gmail.com>
2019-10-04 21:06:06 +1000

    Changed some settings to deploy app as Azure App Service

---
+++ MyAddressBookPlus/Data/InitSchema.sql
@@ -0,0 +1,2 @@
+﻿DROP TABLE IF EXISTS contact;
+Create table contact (Id int NOT NULL IDENTITY(1, 1),Name varchar(50), Email varchar(50), Phone varchar(15), Address varchar(100), PictureName varchar(100));
